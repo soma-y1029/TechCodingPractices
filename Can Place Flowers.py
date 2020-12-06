f712abd6c1a720bfca78b5342a31f9346ef086db
Soma Yoshida <57809819+soma-y1029@users.noreply.github.com>
2020-12-05 20:26:57 -0800

    Create Can Place Flowers.py

---
+++ Can Place Flowers.py
@@ -0,0 +1,26 @@
+https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/569/week-1-december-1st-december-7th/3555/
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        i = 0
+        while i < len(flowerbed):
+            #print(f'{i=}, {n=}, {flowerbed=}')
+            if flowerbed[i]:
+                i += 2
+                continue
+                
+            if not flowerbed[i]:
+                left = flowerbed[i-1] if i > 0 else flowerbed[0]
+                right = flowerbed[i+1] if i < len(flowerbed)-1 else 0
+                #print(left, right)
+                if not left and not right:
+                    flowerbed[i] = 1
+                    n -= 1
+                    i += 2 
+                    continue
+                i += 1
+            
+        if n <= 0:
+            return True
+            
+        return False
