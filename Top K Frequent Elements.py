577fd56b3f469a2ec4fc05150d1ed2741b8653f7
Soma Yoshida <57809819+soma-y1029@users.noreply.github.com>
2020-12-29 07:47:34 -0800

    Update Top K Frequent Elements.py

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -1,6 +1,16 @@
 https://leetcode.com/problems/top-k-frequent-elements/
 
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = collections.Counter(nums).most_common(k)
-        return [item[0] for item in counter]
+        counter = collections.Counter(nums)
+        #return [item[0] for item in counter.items()][:k]
+        arr = []
+        for item in counter.items():
+            arr.append((-item[1], item[0]))
+        heapq.heapify(arr)
+        res = []
+        for i in range(k):
+            res.append(heappop(arr)[1])
+            
+        return res
